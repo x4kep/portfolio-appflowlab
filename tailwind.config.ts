e5254123a26cbaa88d411c0e30b8dfa0f398dca4
Dusan Veselinovic <dusan.veselinovic.dev@gmail.com>
2025-05-23 13:12:02 +0200

    Init: Initial hello world page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,12 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        colors: {
+          black: {
+            DEFAULT: '#000',
+            100: '#000319'
+          }
+        }
       },
     },
   },
