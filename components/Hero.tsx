144d76d3665bea3027a52eecd1c5f40c04704077
Dusan Veselinovic <dusan.veselinovic.dev@gmail.com>
2025-05-24 12:20:27 +0200

    Des: Text intro

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            create saas react native applications in days
+            create saas react native applications in month
           </p>
 
           {/**
@@ -36,12 +36,12 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="From Vision to Value: Designing Better SaaS"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Dusan, a Next.js Developer based in Serbia.
+            Hi! I&apos;m Dusan, a ReactNative Developer based in Serbia.
           </p>
 
           <a href="#about">
