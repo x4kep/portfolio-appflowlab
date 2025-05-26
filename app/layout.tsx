1164f38809ebf24dba22fbfba1853fb7de6b5145
Dusan Veselinovic <dusan.veselinovic.dev@gmail.com>
2025-05-26 11:51:46 +0200

    Des: Intro text

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "App flow lab",
+  title: "Appflowlab",
   description: "Create SaaS React Native aplications in days",
 };
 
--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title: "Collaborate closely with clients to plan MVPs, design UI/UX, conduct research, build SaaS solutions, write clean code, and continuously improve the product.",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
@@ -49,8 +49,8 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
-    description: "The Inside Scoop",
+    title: "Currently building ReactNative application for studing fast",
+    description: "The Inside of appflowlab",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
