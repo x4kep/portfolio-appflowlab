818fc88c7d5cb641ad6060ccd4d44d92521a570c
Dusan Veselinovic <dusan.veselinovic.dev@gmail.com>
2025-06-27 19:36:52 +0200

    Des: Fix

--- data/index.ts
+++ data/index.ts
@@ -108,19 +108,19 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Collaborating with Dusan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dusan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Dusan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dusan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Dusan is the ideal partner.",
     name: "David Edwards",
     title: "Director of Automed",
   },
   {
     quote:
-      "Collaborating with Dusan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dusan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Dusan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dusan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Dusan is the ideal partner.",
     name: "David Edwards",
     title: "Director of Automed",
   },
   {
     quote:
-      "Collaborating with Dusan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dusan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Dusan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dusan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Dusan is the ideal partner.",
     name: "David Edwards",
     title: "Director of Automed",
   },
